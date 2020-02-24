bdc805ec6ee76a66b2a0141cd9bdd9bfdacd6c31
AntonPytsko <pytko_2002@mail.ru>
2020-02-24 18:36:31 +0300

    second commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Пр1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adres ADR = null;
+            string gorod = new string(' ', 20);
+            string ylica = new string(' ', 20);
+            int nomer_doma = new int();
+            Console.WriteLine("Введите название города");
+            gorod = Console.ReadLine();
+            Console.WriteLine("Введите название улицы");
+            ylica = Console.ReadLine();
+            Console.WriteLine("Введите номер дома");
+            nomer_doma = Convert.ToInt32(Console.ReadLine());
+            ADR = new Adres(gorod, ylica, nomer_doma);
+            Console.WriteLine("Обьект созда");
+        }
+    }
+}
